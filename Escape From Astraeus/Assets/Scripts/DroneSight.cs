863fd6ddb7beeb2e8d3f2e0fa03ce1384488dc7d
Christian <cgulick@student.fitchburgstate.edu>
2022-10-01 21:06:59 -0400

    Enemy vision script

--- Escape From Astraeus/Assets/Scripts/DroneSight.cs
+++ Escape From Astraeus/Assets/Scripts/DroneSight.cs
@@ -4,22 +4,75 @@ using UnityEngine;
 
 public class DroneSight : MonoBehaviour
 {
-    public float rayLength = 10.0f;
-    public Vector3 rayAngle;
-    public Vector3 rayAngle2;
+    // Code from https://www.youtube.com/watch?v=j1-OyLo77ss
+    public float radius;
+    [Range(0,360)]
+    public float angle;
+
+    public GameObject playerRef;
+
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+    public bool canSeePlayer;
     
     
     void Start()
     {
-        
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(FOVRoutine());
     }
 
     // Update is called once per frame
     void Update()
     {
-        //rayAngle = rayAngle * Mathf.Deg2Rad;
-        Debug.DrawRay(transform.position, transform.TransformDirection(rayAngle) * rayLength, Color.red);
-        Debug.DrawRay(transform.position, transform.TransformDirection(rayAngle2) * rayLength, Color.red);
+      
+
+    }
+
+    private IEnumerator FOVRoutine()
+    {
+        float delay = 0.2f;
+        WaitForSeconds wait = new WaitForSeconds(delay);
+
+        while(true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if(rangeChecks.Length !=0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if(Vector3.Angle(transform.forward, directionToTarget) < angle /2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if(!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                {
+                    canSeePlayer = true;
+                }
+                else
+                {
+                    canSeePlayer = false;
+                }
+            }
+            else
+            {
+                canSeePlayer = false;
+            }
+        }
+
+        else if(canSeePlayer)
+        {
+            canSeePlayer = false;
+        }
     }
 
     
