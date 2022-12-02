1b3cfd581a3a6f11325a1cfc84e0b6bce66ee4dc
Christian <cgulick@student.fitchburgstate.edu>
2022-12-01 23:45:23 -0500

    Crate Update

--- Escape From Astraeus/Assets/Scripts/Magnitization.cs
+++ Escape From Astraeus/Assets/Scripts/Magnitization.cs
@@ -11,13 +11,16 @@ public class Magnitization : MonoBehaviour
     public int hitDistance;
     public GameObject playerController;
     private PlayerController playerControllerScript;
-    public bool crateInView, holdingCrate;
-    public int moveDistance;
+    [SerializeField]private bool crateInView, holdingCrate;
+    [SerializeField]private int moveDistanceZ, moveDistanceX;
     public NavMeshSurface navMesh;
+    [SerializeField]private bool reloading, on;
+     [SerializeField]private string moveDirection;
 
     // Start is called before the first frame update
     void Start()
     {
+        on = true;
         playerControllerScript = playerController.GetComponent<PlayerController>();
         //navMesh.BuildNavMesh();
     }
@@ -25,41 +28,80 @@ public class Magnitization : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
+        if (on)
+        {
+            RaycastHit hit;
 
-        if(Physics.Raycast(rayStartPoint.transform.position, transform.TransformDirection(Vector3.forward), out hit,  Mathf.Infinity, layermask))
-        {
-            Debug.DrawRay(rayStartPoint.transform.position, transform.TransformDirection(Vector3.forward) * hitDistance, Color.blue);
-            if(hit.collider.gameObject.tag == "Crate")
+            if(Physics.Raycast(rayStartPoint.transform.position, transform.TransformDirection(Vector3.forward), out hit,  Mathf.Infinity, layermask))
             {
-                 crateInView = true;
+                Debug.DrawRay(rayStartPoint.transform.position, transform.TransformDirection(Vector3.forward) * hitDistance, Color.blue);
+                if(hit.collider.gameObject.tag == "CrateZ")
+                {
+                    crateInView = true;
+                    moveDirection = "Z";
+                }
+
+                 if(hit.collider.gameObject.tag == "CrateX")
+                {
+                    crateInView = true;
+                    moveDirection = "X";
+                }
+            
+                //Debug.Log("Hit Crate");
             }
-           
-            //Debug.Log("Hit Crate");
-        }
-        else
-        {
-            //Debug.Log("Hit Crate");
-            crateInView = false;
+            else
+            {
+                //Debug.Log("Hit Crate");
+                crateInView = false;
+            }
+        
+            
+
+
+
+            if(playerControllerScript.Interact.triggered && crateInView == true)
+            {
+                switch (moveDirection)
+                {
+                    case "Z":
+                        StartCoroutine(ReloadMagni());
+                        hit.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y , hit.transform.position.z + moveDistanceZ * Time.deltaTime);
+                    break;
+                    case "X":
+                        StartCoroutine(ReloadMagni());
+                        hit.transform.position = new Vector3(hit.transform.position.x + moveDistanceX * Time.deltaTime, hit.transform.position.y , hit.transform.position.z);
+                    break;
+                }
+                
+                //navMesh.BuildNavMesh();
+                
+            }
+
+            if(playerControllerScript.BotSwitch.triggered && crateInView == true)
+            {
+                  switch (moveDirection)
+                {
+                    case "Z":
+                        StartCoroutine(ReloadMagni());
+                        hit.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y , hit.transform.position.z + moveDistanceZ * Time.deltaTime);
+                    break;
+                    case "X":
+                        StartCoroutine(ReloadMagni());
+                        hit.transform.position = new Vector3(hit.transform.position.x + moveDistanceX * Time.deltaTime, hit.transform.position.y , hit.transform.position.z);
+                    break;
+                }
+            }
         }
        
-        
-
-
-
-        if(playerControllerScript.Interact.triggered && crateInView == true)
-        {
-            //holdingCrate = true;
-            hit.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y , hit.transform.position.z + moveDistance * Time.deltaTime);
-            //navMesh.BuildNavMesh();
-            
-        }
-
-        if(playerControllerScript.BotSwitch.triggered && crateInView == true)
-        {
-            hit.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y , hit.transform.position.z - moveDistance * Time.deltaTime);
-            //navMesh.BuildNavMesh();
-        }
-}
     }
+
+    IEnumerator ReloadMagni()
+    {
+        on = false;
+        crateInView = false;
+        yield return new WaitForSeconds(1f);
+        on = true;
+        StopCoroutine(ReloadMagni());
+    }
+}
 
