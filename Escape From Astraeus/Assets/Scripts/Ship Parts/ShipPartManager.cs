dc5e998a76f8adf825f959866a7ef511a4cce524
Christian <cgulick@student.fitchburgstate.edu>
2022-12-03 19:11:44 -0500

    ship parts and script

---
+++ Escape From Astraeus/Assets/Scripts/Ship Parts/ShipPartManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipPartManager : MonoBehaviour
+{
+    [SerializeField]private GameObject[] shiparts;
+    [SerializeField]private GameObject[] partSpawnLocations;
+    [SerializeField]private bool[] spawnLocationActive;
+    [SerializeField]private int k,i;
+     [SerializeField]private bool randomizedSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnShipParts();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+      
+    }
+
+    void SpawnShipParts()
+    {
+         for (i = 0; i<shiparts.Length; i++)
+       {
+        RandomSpawnPos();
+        Instantiate(shiparts[i], partSpawnLocations[k].transform.position, Quaternion.identity);
+       }
+    }
+    void RandomSpawnPos()
+    {
+        
+        k = Random.Range(0, partSpawnLocations.Length);
+        if (spawnLocationActive[k])
+        {
+            RandomSpawnPos();
+        }
+        else
+        {
+            spawnLocationActive[k] = true;
+        }
+        
+            
+  
+    }
+
+
+}
