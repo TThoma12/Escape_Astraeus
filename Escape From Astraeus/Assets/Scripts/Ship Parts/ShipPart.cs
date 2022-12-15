dc7d2a3f6ccc342316e431948fe6b4437039d0da
Christian <cgulick@student.fitchburgstate.edu>
2022-12-15 12:58:20 -0500

    updated ship part

--- Escape From Astraeus/Assets/Scripts/Ship Parts/ShipPart.cs
+++ Escape From Astraeus/Assets/Scripts/Ship Parts/ShipPart.cs
@@ -9,6 +9,9 @@ public class ShipPart : MonoBehaviour
     public int shipPart_ID;
     public GameObject playerController;
     private PlayerInventory playerInventory;
+    [SerializeField] AudioSource LocaIndicatorSFX;
+    
+
     void Start()
     {
         playerController = GameObject.Find("PlayerController");
@@ -25,7 +28,10 @@ public class ShipPart : MonoBehaviour
     {
         if(collider.gameObject.tag == "Player")
         {
+            playerInventory.playSFX();
+             //pickSFX.Play();
             Update_Player_Inv(shipPart_ID);
+           
             Destroy(gameObject);
         }
     }
@@ -33,5 +39,7 @@ public class ShipPart : MonoBehaviour
     void Update_Player_Inv(int shipPart)
     {
               playerInventory.Player_Ship_Parts[shipPart] = true;
+              //playerInventory.playSFX();
     }
+    
 }
